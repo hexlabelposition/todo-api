8220ce72735e1b9f482b1085e34fd80785232ad4
Vladyslav <129685708+hexlabelposition@users.noreply.github.com>
2024-10-01 10:19:21 +0200

    added simple compression

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import usersRouter from './components/users/users.router.js';
+import compression from './middleware/compression.middleware.js'
 import errorHandler from './middleware/errorHandler.middleware.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(compression)
 app.use("/api/users", usersRouter);
 app.use(errorHandler);
 
